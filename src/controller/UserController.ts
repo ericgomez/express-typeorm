4233cfd233a207e743a1a24596013f8170ad2702
Eric Gomez <eric.gomez.sanchez@gmail.com>
2023-01-27 13:30:04 -0600

    Creating user controller

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,53 +1,111 @@
-import { AppDataSource } from '../data-source'
-import { NextFunction, Request, Response } from "express"
-import { User } from "../entity/User"
+import { NextFunction, Request, Response } from 'express';
+import { validate } from 'class-validator';
+import { AppDataSource } from './../data-source';
+import { User } from './../entity/User';
 
-export class UserController {
+class UserController {
+  static getAll = async (req: Request, res: Response, next: NextFunction) => {
+    const userRepository = AppDataSource.getRepository(User);
+    try {
+      const users = await userRepository.find();
 
-    private userRepository = AppDataSource.getRepository(User)
+      if (users.length > 0) {
+        res.send(users);
+      } else {
+        res.status(404).json({ message: 'Not result' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
 
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find()
+  static getById = async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+
+    const userRepository = AppDataSource.getRepository(User);
+    try {
+      const user = await userRepository.findOneOrFail({
+        where: { id },
+      });
+      res.send(user);
+    } catch (error) {
+      res.status(404).json({ message: 'Not result' });
+    }
+  };
+
+  static newUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { username, password, role } = req.body;
+    const user = Object.assign(new User(), {
+      username,
+      password,
+      role,
+    });
+
+    // validations
+    const errors = await validate(user);
+    if (errors.length > 0) {
+      return res.status(400).json({ message: 'Validation error' });
     }
 
-    async one(request: Request, response: Response, next: NextFunction) {
-        const id = parseInt(request.params.id)
-        
+    // TODO: HASH PASSWORD
 
-        const user = await this.userRepository.findOne({
-            where: { id }
-        })
-
-        if (!user) {
-            return "unregistered user"
-        }
-        return user
+    const userRepository = AppDataSource.getRepository(User);
+    try {
+      await userRepository.save(user);
+    } catch (error) {
+      return res.status(409).json({ message: 'Username already exist' });
     }
 
-    async save(request: Request, response: Response, next: NextFunction) {
-        const { firstName, lastName, age } = request.body;
+    // All Ok
+    res.status(201).json({ message: 'User created' });
+  };
 
-        const user = Object.assign(new User(), {
-            firstName,
-            lastName,
-            age
-        })
+  static editUser = async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+    const { username, role } = req.body;
+    let user;
 
-        return this.userRepository.save(user)
+    const userRepository = AppDataSource.getRepository(User);
+    try {
+      user = await userRepository.findOneByOrFail({ id });
+      // We change the data from the database to that of the request body
+      user.username = username;
+      user.role = role;
+    } catch (error) {
+      return res.status(404).json({ message: 'User not found' });
     }
 
-    async remove(request: Request, response: Response, next: NextFunction) {
-        const id = parseInt(request.params.id)
-
-        let userToRemove = await this.userRepository.findOneBy({ id })
-
-        if (!userToRemove) {
-            return "this user not exist"
-        }
-
-        await this.userRepository.remove(userToRemove)
-
-        return "user has been removed"
+    const errors = await validate(user);
+    if (errors.length > 0) {
+      return res.status(400).json(errors);
     }
 
+    try {
+      await userRepository.save(user);
+    } catch (error) {
+      return res.status(409).json({ message: 'Username already in use' });
+    }
+
+    // All Ok
+    res.status(201).json({ message: 'User update' });
+  };
+
+  static deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+
+    let user: User;
+    const userRepository = AppDataSource.getRepository(User);
+
+    try {
+      user = await userRepository.findOneByOrFail({ id });
+    } catch (error) {
+      return res.sendStatus(404).json({ message: 'User not found' });
+    }
+
+    // Delete user from database
+    await userRepository.remove(user);
+    res.status(200).json({ message: 'User deleted' });
+  };
 }
+
+export default UserController;
