c7964a31f6a2c648af99cdf547831b434f227ef1
Eric Gomez <eric.gomez.sanchez@gmail.com>
2023-01-27 13:34:15 -0600

    Creating auth controller and add in the router

---
+++ src/controller/AuthController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { AppDataSource } from './../data-source';
+import { User } from './../entity/User';
+
+class AuthController {
+  static login = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    if (!(username && password)) {
+      return res.status(400).json({ message: 'Username and password are required!' });
+    }
+
+    const userRepository = AppDataSource.getRepository(User);
+
+    try {
+      const user = await userRepository.findOneOrFail({ where: { username } });
+      res.send(user);
+    } catch (error) {
+      return res.status(404).json({ message: 'Username or password is incorrect!' });
+    }
+  };
+}
+
+export default AuthController;
