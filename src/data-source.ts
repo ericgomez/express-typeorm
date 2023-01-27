7fd1cba2ac24db327d289dd17968cca188ce2220
Eric Gomez <eric.gomez.sanchez@gmail.com>
2023-01-27 09:17:21 -0600

    Configuring file tsconfig.json

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { User } from './entity/User';
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: 'mysql',
+  host: 'db4free.net',
+  port: 3306,
+  username: 'kjb6vhareb',
+  password: 'rrRMJG#.g@nSh8*',
+  database: 'kzq5gchfxvuic2t',
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
