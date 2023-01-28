19cf2ef628836f1df2637ca704c04ba183295351
Eric Gomez <eric.gomez.sanchez@gmail.com>
2023-01-28 10:23:31 -0600

    Implementation of new route change password

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
+import { validate } from 'class-validator';
 import { AppDataSource } from './../data-source';
 import { User } from './../entity/User';
 import config from '../config/config';
@@ -28,6 +29,42 @@ class AuthController {
     } catch (error) {
       return res.status(404).json({ message: 'Username or password is incorrect!' });
     }
+  };
+
+  static changePassword = async (req: Request, res: Response) => {
+    const { userId } = res.locals.jwtPayload;
+    const { oldPassword, newPassword } = req.body;
+
+    if (oldPassword && newPassword) {
+      res.status(400).json({ message: 'Old password and new password are not allowed!' });
+    }
+
+    const userRepository = AppDataSource.getRepository(User);
+    let user: User;
+
+    try {
+      user = await userRepository.findOneOrFail(userId);
+    } catch (error) {
+      res.status(400).json({ message: 'Something goes wrong!' });
+    }
+
+    if (!user.checkPassword(oldPassword)) {
+      return res.status(400).json({ message: 'Old password is incorrect!' });
+    }
+
+    user.password = newPassword;
+    // validate by of class-validator
+    const errors = await validate(user, { validationError: { target: false, value: false } });
+
+    if (errors.length > 0) {
+      return res.status(400).json(errors);
+    }
+
+    // Hash password
+    user.hashPassword();
+    userRepository.save(user);
+
+    res.status(200).json({ message: 'Password changed successfully!' });
   };
 }
 
